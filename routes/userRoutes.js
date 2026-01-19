4e14b22034afdb51b52f2403b1110d9513ad00b8
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-01-19 16:17:38 +0200

    add routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const userController = require(`../controllers/userController`);
+const authController = require(`../controllers/authController`);
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+
+router.route('/').get(userController.getAllUsers).post(userController.addUser)
+
+router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
+
+module.exports = router
