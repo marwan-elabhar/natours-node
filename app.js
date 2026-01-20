0bcffbf668fd7601f2a0e08f33e76860f1867bc9
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-01-20 12:03:04 +0200

    implement rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,9 +11,16 @@ const app = express();
 
 // MIDDLEWARES
 if(process.env.NODE_ENV === 'development') {
-  console.log('NODE_ENV=development');
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100, // requests
+  windowMs: 60 * 60 * 1000, // time
+  message: 'Too many requests. Please try in an hour'
+})
+
+app.use('/api', limiter)
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
