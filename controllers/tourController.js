b67bfb447f4c456db2dbb8aefbaf0cce1e1de248
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-11 09:40:14 +0200

    populate reviews

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -45,7 +45,7 @@ exports.addTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate('reviews')
 
   if (!tour) {
    return next(new AppError('Tour not found', 404));
