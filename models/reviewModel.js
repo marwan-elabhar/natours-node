622404c4bd0ee6f68e87417a7df5086c5425040c
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-11 09:38:59 +0200

    add reviewModel

---
+++ models/reviewModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+const reviewSchema = new mongoose.Schema({
+        review: {
+            type: String,
+            required: [true, 'Review is required'],
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+        },
+        createdAt: {type: Date, default: Date.now(), select: false},
+        tour: {type: mongoose.Schema.ObjectId, ref: 'Tour', required: [true, 'Review must belong to a tour.']},
+        user: {type: mongoose.Schema.ObjectId, ref: 'User', required: [true, 'Review is required']},
+
+    },
+    {
+        toJSON: {virtuals: true},
+        toObject: {virtuals: true}
+    })
+
+reviewSchema.pre(/^find/, function (req, res, next) {
+    // this.populate({ path: 'tour', select: 'name'}).populate({ path: 'user', select: 'name photo'})
+
+    this.populate({ path: 'user', select: 'name photo'})
+
+    next()
+})
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
