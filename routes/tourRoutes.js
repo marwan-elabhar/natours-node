945c58bc3a443fb10b893e21508f2995885d4774
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-11 10:16:35 +0200

    add middlerware in tourRouters

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewRouter = require("./reviewRoutes")
 const router = express.Router();
 
 // router.param('id', tourController.checkID)
+
+router.use('/:tourId/reviews',reviewRouter)
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours,tourController.getAllTours)
 router.route('/tour-stats').get(tourController.getTourStats)
@@ -19,5 +22,8 @@ router
   .route('/:id')
   .get(tourController.getTour)
   .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
+
+// router.route('/tourId/reviews').post(authController.protect, authController.restrictTo('user'),
+//     reviewController.createReview)
 
 module.exports = router;
