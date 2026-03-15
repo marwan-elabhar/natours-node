4c0bd50acb8b8a6e150f197ea0e88dcf2ccd1729
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-15 11:15:33 +0200

    replace deleteUser with factory function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require("../utils/appError");
+const factory = require('./handleFactory')
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {}
@@ -65,12 +66,9 @@ exports.updateUser = (req, res) => {
   })
 }
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: "Internal Server Error"
-  })
-}
+
+
+exports.deleteUser = factory.deleteOne(User)
 
 exports.deleteMe = catchAsync(async (req, res) => {
   await User.findByIdAndUpdate(req.user.id, { active: false})
