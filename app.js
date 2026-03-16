c6fe4998e4cdebe72136d841ebf42f9f927e10b6
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-16 13:05:32 +0200

    import reviewRouter

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
+const hpp = require('hpp');
+
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 /// set security https headers
-app.use(helmet());
+// app.use(helmet());
 
 
 // MIDDLEWARES
@@ -28,6 +33,19 @@ app.use('/api', limiter)
 
 // body parser, reading data from body into req.body
 app.use(express.json({limit: '10mb'}));
+
+// data sanitization against NoSQL query injection
+// app.use(mongoSanitize())
+
+
+// data sanitization against XSS
+// app.use(xss())
+//
+// // Prevent parameter pollution
+// app.use(hpp({
+//   whitelist: ['duration']
+// }))
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -44,6 +62,7 @@ app.use((req,res,next) => {
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/reviews', reviewRouter)
 
 app.use((req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl}`, 400));
