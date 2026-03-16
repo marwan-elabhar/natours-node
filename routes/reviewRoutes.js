c188fbee03f5b01c2bb609601bb8636599a16516
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-16 13:03:50 +0200

    modify reviewController and reviewRoutes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,9 +4,9 @@ const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
 
-router.route('/').get(reviewController.getAllReviews).post(authController.protect, authController.restrictTo('user'),reviewController.createReview);
+router.route('/').get(reviewController.getAllReviews).post(authController.protect, authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview);
 
-router.route('/:id').delete(reviewController.deleteReview)
+router.route('/:id').delete(reviewController.deleteReview).patch(reviewController.updateReview)
 
 module.exports = router;
 
