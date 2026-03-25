46c207e3f6852f1e0055d2baf24ddb3f9f066027
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-25 09:50:12 +0200

    authentication and authorization updates

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,17 +10,18 @@ router.use('/:tourId/reviews',reviewRouter)
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours,tourController.getAllTours)
 router.route('/tour-stats').get(tourController.getTourStats)
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+router.route('/monthly-plan/:year').get(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.getMonthlyPlan)
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.addTour);
+  .get(tourController.getAllTours)
+  .post(authController.protect, authController.restrictTo('admin', 'lead-guide'),tourController.addTour);
 
 
 router
   .route('/:id')
   .get(tourController.getTour)
+    .patch(authController.protect, authController.restrictTo('admin', 'lead-guide'),tourController.updateTour)
   .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 // router.route('/tourId/reviews').post(authController.protect, authController.restrictTo('user'),
