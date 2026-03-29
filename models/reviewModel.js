5cc20be082d4c6fc964bfe7af85c3b44637bfbfd
marwan-elabhar <marwan.a.elabhar@gmail.com>
2026-03-29 11:51:53 +0200

    implement ratings avg

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -29,6 +29,42 @@ reviewSchema.pre(/^find/, function ( next) {
     next()
 })
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+   const stats = await this.aggregate([
+        {
+            $match: {tour: tourId}
+        },
+        {
+            $group: {
+                _id: '$tourId',
+                nRating: {$sum: 1},
+                avgRating: {$avg: '$rating'}
+            }
+        }
+    ])
+    console.log(stats)
+    if(stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {ratingsQuantity: stats[0].nRating, ratingsAverage: stats[0].avgRating})
+
+    }
+}
+
+reviewSchema.index({ tour: 1, user: 1}, {unique: true})
+
+reviewSchema.post('save', function () {
+    this.constructor.calcAverageRatings(this.tour)
+})
+
+reviewSchema.pre(/findOneAnd/, async function(next) {
+    const r = await this.findOne(next)
+    next()
+})
+
+reviewSchema.post(/findOneAnd/, async function () {
+  await  this.r.constructor.calcAverageRatings(this.r.tour)
+})
+
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
